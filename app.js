1866770a88e58970f151f7594d81393693ccc233
Sultan <romtrash98@gmail.com>
2020-02-29 15:15:36 +0300

    Добавлено подключение к бд

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const config = require('config');
+const { Sequelize } = require('sequelize');
 
 const app = express();
+const sequelize = new Sequelize(config.get('postgresUri'));
 
 const PORT = config.get('port') || 5000;
+
+async function start() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.')
+    } catch (e) {
+        console.log('Server Error:', e.message);
+        process.exit(1);
+    }
+}
+
+start().then(() => {});
 
 app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
