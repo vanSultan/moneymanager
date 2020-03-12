c06d02d56a1ea35a582bf2a5cd9ef9d7320d5161
Sultan <romtrash98@gmail.com>
2020-03-12 15:27:44 +0300

    Изменен файл конфигурации

--- config/config.js
+++ config/config.js
@@ -1,12 +1,52 @@
-const 
+require('dotenv').config();
 
-{
+const {NODE_ENV, JWT_SECRET, POSTGRES_URL} = process.env;
+const env = NODE_ENV || 'dev';
+
+const dev = {
     "app": {
         "port": 5000,
-        "jwtSecret": "its secret code"
+        "jwtSecret": JWT_SECRET || 'its secret code',
     },
     "database": {
-        "postgresUri": "postgres://moneymanager:mmpwd@192.168.10.36:5432/moneymanagerdb",
-        "sqliteUri": "./moneymanagerdb.sqlite3"
+        dialect: 'sqlite',
+        storage: "./moneymanagerdb.sqlite3",
+        define: {
+            timestamps: false
+        }
     }
-}
+};
+
+const test = {
+    "app": {
+        "port": 5000,
+        "jwtSecret": JWT_SECRET || 'its secret code',
+    },
+    "database": {
+        dialect: 'sqlite',
+        storage: "./moneymanagerdb.sqlite3",
+        define: {
+            timestamps: false
+        }
+    }
+};
+
+const prod = {
+    "app": {
+        "port": 5000,
+        "jwtSecret": JWT_SECRET,
+    },
+    "database": {
+        dialect: 'postgres',
+        url: POSTGRES_URL,
+        define: {
+            timestamps: false
+        }
+    }
+};
+
+const config = {
+    dev, test, prod
+};
+
+module.exports = config[env];
