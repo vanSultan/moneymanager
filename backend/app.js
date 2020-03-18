f7fd4df4d3f90074b4e4f1260b5464b6c3a179ce
Igor Spirin <spirinigor98@gmail.com>
2020-03-18 23:26:14 +0300

    Убрана инициализация бд

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { app: appConfig } = require('./config/config');
-const { db } = require('./models');
-const initDatabase = require('./models/init_database');
+// const { db } = require('./models');
+// const initDatabase = require('./models/init_database');
 
 process.env.NODE_CONFIG_DIR = './config';
 
@@ -15,8 +15,8 @@ const PORT = appConfig.port;
 
 async function start() {
   try {
-    await db.sequelize.sync();
-    initDatabase(db.models);
+    // await db.sequelize.sync();
+    // initDatabase(db.models);
     // eslint-disable-next-line no-console
     app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
   } catch (e) {
@@ -27,3 +27,5 @@ async function start() {
 }
 
 start().then(() => {});
+
+module.exports = app;
