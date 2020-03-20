846a5c554d1b33988db6166bd9cbbaea67aa2840
Sultan <40956689+vanSultan@users.noreply.github.com>
2020-03-20 22:28:13 +0300

    Revert "Feature/implement init test"

--- backend/models/init_database/index.js
+++
@@ -1,9 +0,0 @@
-const initRole = require('./role.init');
-const initAccountType = require('./account_type.init');
-const initCategory = require('./category.init');
-
-module.exports = function initDatabase(models) {
-  initRole(models.Role);
-  initAccountType(models.AccountType);
-  initCategory(models.Category);
-};
--- backend/models/init_database/role.init.js
+++
@@ -1,7 +0,0 @@
-module.exports = function initRole(RoleModel) {
-  RoleModel.bulkCreate([
-    { name: 'user' },
-    { name: 'moderator' },
-    // eslint-disable-next-line no-console
-  ]).catch(() => { console.log('Role have already been initialized'); });
-};
