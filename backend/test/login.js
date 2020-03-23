4078795cbd8e4cd82eb56a7ead849a699622c1fd
Igor Spirin <spirinigor98@gmail.com>
2020-03-24 00:05:33 +0300

    #15 Добавлена проверка авторизации существующего пользователя

--- backend/test/login.js
+++ backend/test/login.js
@@ -3,6 +3,7 @@ const chaiHttp = require('chai-http');
 const server = require('../app');
 
 chai.should();
+chai.use(chaiHttp);
 
 const user = {
   login: 'not existing user',
@@ -24,7 +25,7 @@ describe('/api/auth/login', () => {
 
   it('it should return an error for invalid password', (done) => {
     user.login = 'existing_user';
-    user.password = 'existing_password';
+    user.password = 'not_existing_password';
     chai.request(server)
       .post('/api/auth/login/')
       .send(user)
@@ -35,6 +36,18 @@ describe('/api/auth/login', () => {
         done();
       });
   });
-});
 
-chai.use(chaiHttp);
+  it('it should login successfully', (done) => {
+    user.login = 'new user';
+    user.password = 'new password';
+    chai.request(server)
+      .post('/api/auth/login')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('token');
+        done();
+      });
+  });
+});
