f573b1cdd3030ecb42ef8734433d1af1fd71cb71
Igor Spirin <spirinigor98@gmail.com>
2020-03-29 18:35:40 +0300

    #13 Функция проверки отношений Сущность-Пользователь

---
+++ backend/controllers/entities.controller.js
@@ -0,0 +1,20 @@
+const { models } = require('../models');
+
+const { ExternalEntityUser } = models;
+
+/*
+Проверяет существует ли связь между пользователем и сущностью
+Возвращает сущность связи, если она существует, иначе undefined
+ */
+async function checkEntityUserConnection(entityId, userId) {
+  if (entityId === undefined || userId === undefined) {
+    throw new Error('Undefined arguments');
+  }
+
+  return ExternalEntityUser.findOne({
+    where: {
+      user_id: userId,
+      external_entity_id: entityId,
+    },
+  });
+}
