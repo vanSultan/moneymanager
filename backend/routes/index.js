3f0f0c8f190c3b2a3ac7f91efdebd6f2634984f1
Igor Spirin <spirinigor98@gmail.com>
2020-03-29 21:07:52 +0300

    #13 Добавлены руты для сущностей

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 const authRouter = require('./auth.routes');
 const usersRouter = require('./users.routes');
+const entitiesRouter = require('./entities.routes');
 
 module.exports = (app) => {
   app.use('/api/auth', authRouter);
   app.use('/api/users', usersRouter);
+  app.user('/api/externalEntities/', entitiesRouter);
 };
