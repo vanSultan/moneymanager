745ffe9b2c00997ca454d4acf2841ee7c0097dce
Igor Spirin <spirinigor98@gmail.com>
2020-03-31 23:44:22 +0300

    #13 Добавлен контроллер для обновления Сущности у пользователя

--- backend/controllers/entities.controller.js
+++ backend/controllers/entities.controller.js
@@ -1,24 +1,6 @@
-const logger = require('../config/logger').appLogger;
 const { models } = require('../models');
 
 const { ExternalEntity, ExternalEntityUser } = models;
-
-/*
-  Проверяет существует ли связь между пользователем и сущностью
-  Возвращает сущность связи, если она существует, иначе null
-*/
-// async function checkEntityUserConnection(entityId, userId) {
-//   if (entityId === null || userId === null) {
-//     throw new Error('Undefined arguments');
-//   }
-//
-//   return ExternalEntityUser.findOne({
-//     where: {
-//       user_id: userId,
-//       external_entity_id: entityId,
-//     },
-//   });
-// }
 
 /*
   Создает связь Пользователь-Внешняя сущность
@@ -36,22 +18,6 @@ async function connectEntityUser(entityId, userId) {
 }
 
 /*
-  Удаляет связь Сущность-Пользователь
- */
-// async function deleteEntityUserConnection(entityId, userId) {
-//   if (entityId === null || userId === null) {
-//     throw new Error('Undefined arguments');
-//   }
-//
-//   return ExternalEntityUser.delete({
-//     where: {
-//       user_id: userId,
-//       external_entity_id: entityId,
-//     },
-//   });
-// }
-
-/*
   Получение id Сущности по имени
   Возвращает Promise<Model>
 */
@@ -67,23 +33,6 @@ async function getEntityIdByName(entityName) {
     },
   });
 }
-
-/*
-  Проверяет существование внешней сущности по id
-  В случае успеха возвращает ее имя, иначе null
-*/
-// async function getEntityNameById(entityId) {
-//   if (entityId === null) {
-//     throw new Error('Undefined arguments');
-//   }
-//
-//   return ExternalEntity.findOne({
-//     attributes: ['name'],
-//     where: {
-//       id: entityId,
-//     },
-//   }).name;
-// }
 
 /*
   Создает новую внешнюю сущность
@@ -135,45 +84,6 @@ async function addEntityToUser(entityName, userId) {
     });
 }
 
-// async function checkEntityConnections(entityId) {
-//   const connections = ExternalEntityUser.findAll({
-//     where: {
-//       external_entity_id: entityId,
-//     },
-//   });
-//
-//   return connections.length !== 0;
-// }
-//
-// async function deleteExternalEntity(entityId) {
-//   return ExternalEntity.delete({
-//     where: {
-//       id: entityId,
-//     },
-//   });
-// }
-
-/*
-  Удаляет связь Сущность-Пользователь по id
-  Возвращет true, если связь была удалена, иначе false
-*/
-// async function deleteEntityUser(entityId, userId) {
-//   if (entityId === null || userId === null) {
-//     throw new Error('Undefined arguments');
-//   }
-//
-//   if (await checkEntityUserConnection(entityId, userId) !== null) {
-//     await deleteEntityUserConnection(entityId, userId);
-//     if (await checkEntityConnections(entityId) === false) {
-//       await deleteExternalEntity(entityId);
-//     }
-//   } else {
-//     return false;
-//   }
-//
-//   return true;
-// }
-
 /*
   Получает информацию о внешней сущности пользователя по ее id и id пользователя.
   Возвращает Promise<Model>
@@ -196,31 +106,6 @@ async function getEntityInfo(entityId, userId) {
   });
 }
 
-// async function updateEntityUserRelation(entityId, newEntityId) {
-//   return ExternalEntityUser.update({
-//     external_entity_id: newEntityId,
-//   }, {
-//     where: {
-//       external_entity_id: entityId,
-//     },
-//   });
-// }
-
-// async function updateEntityName(entityId, newEntityName, userId) {
-//   if (entityId === null || newEntityName === null) {
-//     throw new Error('Undefined arguments');
-//   }
-//
-//   if (await checkEntityUserConnection(entityId, userId) !== null) {
-//     let newEntityId = await getEntityIdByName(newEntityName);
-//     if (newEntityId === null) {
-//       newEntityId = await createExternalEntity(newEntityName);
-//     }
-//     return await updateEntityUserRelation(entityId, newEntityId) !== null;
-//   }
-//   return false;
-// }
-
 /*
   Получает внешние сущности пользователя
   Возвращвет Promise<Array<Model>>
@@ -242,8 +127,37 @@ async function getUserEntities(userId) {
   });
 }
 
+/*
+  Обновляет название Сущности у пользователя
+  Возвращает Promise<Array<number,number>>
+*/
+async function updateUserEntity(entityId, newEntityName, userId) {
+  if (entityId === null || newEntityName === null || userId === null) {
+    throw new Error('Нулевые аргументы функции');
+  }
+
+  return ExternalEntity.findOrCreate({
+    where: {
+      name: newEntityName,
+    },
+  }).then((newEntity) => {
+    const newEntityId = newEntity[0].id;
+    return ExternalEntityUser.update({
+      external_entity_id: newEntityId,
+    }, {
+      where: {
+        user_id: userId,
+        external_entity_id: entityId,
+      },
+    });
+  }).catch(() => {
+    throw new Error('Не удалось обновить пользовательскую сущность');
+  });
+}
+
 module.exports = {
   addEntityToUser,
   getUserEntities,
   getEntityInfo,
+  updateUserEntity,
 };
