147f8c38391089afe6bac61290fdf3502c8de5c5
Sultan <40956689+vanSultan@users.noreply.github.com>
2020-04-22 23:58:40 +0300

    Revert "Feature/implement api accounts"

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,7 @@
 const authRouter = require('./auth.routes');
 const usersRouter = require('./users.routes');
-const accountsRouter = require('./accounts.routes');
 
 module.exports = (app) => {
   app.use('/api/auth', authRouter);
   app.use('/api/users', usersRouter);
-  app.use('/api/accounts', accountsRouter);
 };
