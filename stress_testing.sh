bb7b7a3ddc480d06372f8d49c4d0b5b60b636eb3
Sultan Zhumabaev <romtrash98@gmail.com>
2020-04-30 01:20:29 +0300

    #50 Добавлен скрипт для нагрузочного тестирования

--- stress_testing.sh
+++ stress_testing.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-echo "Stress testing started"
+echo '{"login":"new user","password":"new password"}' > ./user.json
+ab -c 100 -n 10000 -T application/json -p user.json http://localhost:8080/api/auth/login > stress_test
+rm ./user.json
 
-exho "Stress testing ended"
+grep "Requests per second:" stress_test | RPS=$(grep -o -E '[0-9.]+')
+
+echo $RPS
+
