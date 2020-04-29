cd99c4fbfeb3a5bfeca801f6add875d33094ebcf
Sultan Zhumabaev <romtrash98@gmail.com>
2020-04-30 02:36:05 +0300

    Revert "#50 обновлен хост"
    This reverts commit 0ca551785731909c2f7744e15936f0c97bb12eb5.

--- stress_testing.sh
+++ stress_testing.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo '{"login":"new user","password":"new password"}' > ./user.json
-ab -c 100 -n 10000 -T application/json -p user.json http://127.0.0.1:8080/api/auth/login > stress_test
+ab -c 100 -n 10000 -T application/json -p user.json http://localhost:8080/api/auth/login > stress_test
 rm ./user.json
 
 grep "Requests per second:" stress_test | RPS=$(grep -o -E '[0-9.]+')
