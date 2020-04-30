dbd86b3f6ee33d25ea31625e918ec6fff9ef3102
Sultan Zhumabaev <romtrash98@gmail.com>
2020-04-30 22:34:25 +0300

    update quotes

--- stress_testing.sh
+++ stress_testing.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -d '{"login":"stress_user","password":"stress_pwd"}' -H 'Content-Type: application/json' -H 'Accept: application/json' "http://127.0.0.1:\"${NODE_PORT}\"/api/auth/register"
+curl -d '{"login":"stress_user","password":"stress_pwd"}' -H 'Content-Type: application/json' -H 'Accept: application/json' "http://127.0.0.1:${NODE_PORT}/api/auth/register"
 
 echo '{"login":"stress_user","password":"stress_pwd"}' > ./user.json
 ab -c 100 -n 10000 -T application/json -p user.json http://127.0.0.1:"${NODE_PORT}"/api/auth/login > stress_test
