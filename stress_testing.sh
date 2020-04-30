71ab8e8f0bf8c7e75c6a3a186acac5088773dd48
Spirin Igor <new.world.exists@gmail.com>
2020-05-01 00:45:07 +0300

    #50 Добавлена проверка RPS

--- stress_testing.sh
+++ stress_testing.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-curl -d "{\"login\":\"stress_user\",\"password\":\"stress_pwd\"}" -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST http://127.0.0.1:${NODE_PORT}/api/auth/register
-
-echo "{\"login\":\"stress_user\",\"password\":\"stress_pwd\"}" > ./user.json
+#curl -d "{\"login\":\"stress_user\",\"password\":\"stress_pwd\"}" -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST http://127.0.0.1:${NODE_PORT}/api/auth/register
+#
+echo "{\"login\":\"default_user\",\"password\":\"default_password}" > ./user.json
+#echo "{\"login\":\"default_user\",\"password\":\"default_password}"
 RPS=$( ab -c 100 -n 10000 -H 'Content-Type: application/json' -H 'Accept: application/json' -p ./user.json http://127.0.0.1:"${NODE_PORT}"/api/auth/login | grep "Requests per second:" | grep -o -E "[0-9.]+" )
 rm ./user.json
 
-if [ "`echo "${RPS} < 200.0" | bc`" -eq 1 ]; then
+if [ ${RPS} -lt 200 ]; then
   exit 1
 fi
