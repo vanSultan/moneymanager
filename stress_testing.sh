f6417b97a9533dea220c4243af655f92b7eed77d
Sultan Zhumabaev <romtrash98@gmail.com>
2020-04-30 23:07:41 +0300

    fix syntaxys

--- stress_testing.sh
+++ stress_testing.sh
@@ -3,9 +3,9 @@
 curl -d "{\"login\":\"stress_user\",\"password\":\"stress_pwd\"}" -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST http://127.0.0.1:${NODE_PORT}/api/auth/register
 
 echo "{\"login\":\"stress_user\",\"password\":\"stress_pwd\"}" > ./user.json
-ab -c 100 -n 10000 -T application/json -p user.json http://127.0.0.1:"${NODE_PORT}"/api/auth/login > stress_test
+ab -c 100 -n 10000 -T application/json -p user.json http://127.0.0.1:${NODE_PORT}/api/auth/login > stress_test
 rm ./user.json
 
-RPS=$( grep \'Requests per second:\' stress_test | grep -o -E "[0-9.]+" )
+RPS=$( grep "Requests per second:" ./stress_test | grep -o -E "[0-9.]+" )
 
 echo ${RPS}
