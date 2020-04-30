c6b8f98df4f48bfc6a9e842a278e3b1cf9cf1210
Sultan Zhumabaev <romtrash98@gmail.com>
2020-04-30 23:32:49 +0300

    update options

--- stress_testing.sh
+++ stress_testing.sh
@@ -3,7 +3,7 @@
 curl -d "{\"login\":\"stress_user\",\"password\":\"stress_pwd\"}" -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST http://127.0.0.1:${NODE_PORT}/api/auth/register
 
 echo "{\"login\":\"stress_user\",\"password\":\"stress_pwd\"}" > ./user.json
-RPS=$( ab -c 100 -n 10000 -T application/json -p user.json http://127.0.0.1:"${NODE_PORT}"/api/auth/login | grep "Requests per second:" | grep -o -E "[0-9.]+" )
+RPS=$( ab -c 100 -n 10000 -T 'application/json' -H 'Accept: application/json' -p user.json http://127.0.0.1:"${NODE_PORT}"/api/auth/login | grep "Requests per second:" | grep -o -E "[0-9.]+" )
 rm ./user.json
 
 echo ${RPS}
