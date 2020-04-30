295ec23ee7e3bd916c16cc31f34cd0458583232b
Sultan <40956689+vanSultan@users.noreply.github.com>
2020-04-30 21:29:45 +0300

    Добавлена регистрация

--- stress_testing.sh
+++ stress_testing.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-echo '{"login":"default_user","password":"default_password"}' > ./user.json
+curl -d '{"login":"stress_user","password":"stress_pwd"}' -H 'Content-Type: application/json' -H 'Accept: application/json' 
+    http://127.0.0.1:"${NODE_PORT}"/api/auth/register
+
+echo '{"login":"stress_user","password":"stress_pwd"}' > ./user.json
 ab -c 100 -n 10000 -T application/json -p user.json http://127.0.0.1:"${NODE_PORT}"/api/auth/login > stress_test
 rm ./user.json
 
-grep "Requests per second:" stress_test | RPS=$(grep -o -E '[0-9.]+')
+RPS=$( grep "Requests per second:" stress_test | grep -o -E '[0-9.]+' )
 
-echo $RPS
+echo ${RPS}
 
