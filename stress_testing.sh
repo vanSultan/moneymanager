1267f3d0906e6da38c2fbb9ea64ade6491cbfc7e
Sultan Zhumabaev <romtrash98@gmail.com>
2020-04-30 23:12:49 +0300

    change pipeline

--- stress_testing.sh
+++ stress_testing.sh
@@ -3,9 +3,7 @@
 curl -d "{\"login\":\"stress_user\",\"password\":\"stress_pwd\"}" -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST http://127.0.0.1:${NODE_PORT}/api/auth/register
 
 echo "{\"login\":\"stress_user\",\"password\":\"stress_pwd\"}" > ./user.json
-ab -c 100 -n 10000 -T application/json -p user.json http://127.0.0.1:${NODE_PORT}/api/auth/login > stress_test
+RPS=$( ab -c 100 -n 10000 -T application/json -p user.json http://127.0.0.1:${NODE_PORT}/api/auth/login | grep "Requests per second:" | grep -o -E "[0-9.]+" )
 rm ./user.json
-
-RPS=$( grep "Requests per second:" ./stress_test | grep -o -E "[0-9.]+" )
 
 echo ${RPS}
