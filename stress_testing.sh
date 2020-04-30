5787a08c768a9711030a16e89e77597fb8ba84e8
Sultan Zhumabaev <romtrash98@gmail.com>
2020-04-30 22:00:50 +0300

    update quotes

--- stress_testing.sh
+++ stress_testing.sh
@@ -6,6 +6,6 @@ echo '{"login":"stress_user","password":"stress_pwd"}' > ./user.json
 ab -c 100 -n 10000 -T application/json -p user.json http://127.0.0.1:"${NODE_PORT}"/api/auth/login > stress_test
 rm ./user.json
 
-RPS=$( grep "Requests per second:" stress_test | grep -o -E '[0-9.]+' )
+RPS=$( grep \'Requests per second:\' stress_test | grep -o -E \'[0-9.]+\' )
 
 echo ${RPS}
