8ae020cc0c48ed1cc3de198bc95de20db806443e
Sultan Zhumabaev <romtrash98@gmail.com>
2020-05-04 20:01:14 +0300

    #50 изменен путь к интерпретатору

--- stress_testing.sh
+++ stress_testing.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash/
 
 echo "{\"login\":\"default_user\",\"password\":\"default_password}" > ./user.json
 RPS=$( ab -c 100 -n 10000 -H 'Content-Type: application/json' -H 'Accept: application/json' -p ./user.json http://127.0.0.1:"${NODE_PORT}"/api/auth/login | grep "Requests per second:" | grep -o -E "[0-9.]+" )
